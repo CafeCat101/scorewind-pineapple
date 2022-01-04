0348e40640abce653256cece22e50d293b60a289
Leonore <leonore1982@gmail.com>
2022-01-04 21:25:02 +0800

    Initial Commit

---
+++ SWScoreViewerSandbox/SWScoreViewerSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWScoreViewerSandboxApp.swift
+//  SWScoreViewerSandbox
+//
+//  Created by Leonore Yardimli on 2022/1/4.
+//
+
+import SwiftUI
+
+@main
+struct SWScoreViewerSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
